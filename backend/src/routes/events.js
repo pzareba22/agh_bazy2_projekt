8f15622fdea5023c26bfed50ed03dfba32cb44c2
pzareba22 <pzareba22@gmail.com>
2022-05-24 14:04:26 +0200

    formatting and 2 new endpoints

--- backend/src/routes/events.js
+++ backend/src/routes/events.js
@@ -1,108 +1,146 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const EventService = require('../service/eventService')
+const EventService = require("../service/eventService");
 const {
-    ResourceNotFoundError, UserWithThisNameAlreadyExists, AlreadyJoinedThisEvent, AlreadyConfirmedEventParticipation
+    ResourceNotFoundError,
+    UserWithThisNameAlreadyExists,
+    AlreadyJoinedThisEvent,
+    AlreadyConfirmedEventParticipation,
+    IsNotParticipatingInThisEvent,
 } = require("../models/errors");
-const {checkToken} = require("../utils/securityUtils");
+const { checkToken } = require("../utils/securityUtils");
 
+router.get("/", async (req, res, next) => {
+    console.log("Getting all events");
+    try {
+        const { username } = req.params;
+        const eventList = await EventService.getAllEvents(username);
+        res.json(eventList);
+    } catch (e) {
+        res.status(500).send();
+    }
+});
 
-router.get('/',
-    async (req, res, next) => {
-        console.log("Getting all events");
-        try {
-            const {username} = req.params;
-            const eventList = await EventService.getAllEvents(username);
-            res.json(eventList);
-        } catch (e) {
+router.get("/:eventId", async (req, res, next) => {
+    try {
+        const { eventId } = req.params;
+        console.log(`Getting event with id ${eventId}`);
+
+        const event = await EventService.getEventById(eventId);
+        res.json(event);
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
+router.post("/", checkToken, async (req, res, next) => {
+    console.log(`Creating new event`);
+
+    try {
+        const createdEvent = await EventService.createEvent(req.body);
+        res.json(createdEvent);
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
+router.patch("/:eventId", checkToken, async (req, res, next) => {
+    const { eventId } = req.params;
+
+    console.log(`Creating new event`);
+
+    try {
+        const updatedEvent = await EventService.updateEvent(eventId, req.body);
+        res.json(updatedEvent);
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
+router.post("/:eventId/join", checkToken, async (req, res, next) => {
+    const { eventId } = req.params;
+    const { participantUsername } = req.body;
+
+    console.log(
+        `Participant ${participantUsername} is joining event ${eventId}`
+    );
+
+    try {
+        const updatedEventParticipation = await EventService.joinEvent(
+            eventId,
+            participantUsername
+        );
+        res.json(updatedEventParticipation);
+    } catch (e) {
+        if (e instanceof AlreadyJoinedThisEvent) {
+            const { status, message } = e;
+            res.status(status).json({ message });
+        } else {
             res.status(500).send();
         }
-    });
+    }
+});
 
-router.get('/:eventId',
-    async (req, res, next) => {
-
-        try {
-            const {eventId} = req.params;
-            console.log(`Getting event with id ${eventId}`);
-
-            const event = await EventService.getEventById(eventId);
-            res.json(event);
-        } catch (e) {
+router.post("/:eventId/leave", checkToken, async (req, res, next) => {
+    const { eventId } = req.params;
+    const { participantUsername } = req.body;
+    console.log(
+        `Participant ${participantUsername} is leaving event ${eventId}`
+    );
+    try {
+        await EventService.leaveEvent(eventId, participantUsername);
+        console.log("success");
+        res.status(200).send();
+    } catch (e) {
+        if (e instanceof IsNotParticipatingInThisEvent) {
+            const { status, message } = e;
+            res.status(status).json({ message });
+        } else {
             res.status(500).send();
         }
-    });
+    }
+});
 
+router.post("/:eventId/confirm", checkToken, async (req, res, next) => {
+    const { eventId } = req.params;
+    const { participantUsername } = req.body;
 
-router.post('/',
-    checkToken,
-    async (req, res, next) => {
-        console.log(`Creating new event`);
+    console.log(
+        `Participant ${participantUsername} is being confirmed at event ${eventId}`
+    );
 
-        try {
-            const createdEvent = await EventService.createEvent(req.body);
-            res.json(createdEvent);
-        } catch (e) {
+    try {
+        const updatedEventParticipation =
+            await EventService.confirmEventParticipation(
+                eventId,
+                participantUsername
+            );
+        res.json(updatedEventParticipation);
+    } catch (e) {
+        if (
+            e instanceof AlreadyConfirmedEventParticipation ||
+            e instanceof ResourceNotFoundError
+        ) {
+            const { status, message } = e;
+            res.status(status).json({ message });
+        } else {
             res.status(500).send();
         }
-    });
+    }
+});
 
-router.patch('/:eventId',
-    checkToken,
-    async (req, res, next) => {
-        const {eventId} = req.params;
+router.get("/:eventId/participants", checkToken, async (req, res, next) => {
+    const { eventId } = req.params;
+    console.log(`Getting event ${eventId} participants`);
+    res.status(200);
+    data = await EventService.getEventParticipants(eventId);
+    res.json({ data: data });
+});
 
-        console.log(`Creating new event`);
-
-        try {
-            const updatedEvent = await EventService.updateEvent(eventId, req.body);
-            res.json(updatedEvent);
-        } catch (e) {
-            res.status(500).send();
-        }
-    });
-
-router.post('/:eventId/join',
-    checkToken,
-    async (req, res, next) => {
-        const {eventId} = req.params;
-        const {participantUsername} = req.body;
-
-        console.log(`Participant ${participantUsername} is joining event ${eventId}`);
-
-        try {
-            const updatedEventParticipation = await EventService.joinEvent(eventId, participantUsername);
-            res.json(updatedEventParticipation);
-        } catch (e) {
-            if (e instanceof AlreadyJoinedThisEvent) {
-                const {status, message} = e;
-                res.status(status).json({message});
-            } else {
-                res.status(500).send();
-            }
-        }
-    });
-
-router.post('/:eventId/confirm',
-    checkToken,
-    async (req, res, next) => {
-        const {eventId} = req.params;
-        const {participantUsername} = req.body;
-
-        console.log(`Participant ${participantUsername} is being confirmed at event ${eventId}`);
-
-        try {
-            const updatedEventParticipation = await EventService.confirmEventParticipation(eventId, participantUsername);
-            res.json(updatedEventParticipation);
-        } catch (e) {
-            if (e instanceof AlreadyConfirmedEventParticipation || e instanceof ResourceNotFoundError) {
-                const {status, message} = e;
-                res.status(status).json({message});
-            } else {
-                res.status(500).send();
-            }
-        }
-    });
-
+// router.get("/:paticipantUsername", checkToken, async (req, res, next) => {
+//     const { participantUsername } = req.params;
+//     console.log(`getting users: ${participantUsername} events`);
+//     res.status(200).send();
+// });
 
 module.exports = router;
--- backend/src/service/eventService.js
+++ backend/src/service/eventService.js
@@ -1,111 +1,163 @@
-const mongoose = require('mongoose');
-require('../models/Event');
-require('../models/EventParticipation');
+const mongoose = require("mongoose");
+require("../models/Event");
+require("../models/EventParticipation");
 const {
     InvalidLoginCredentialsError,
     ResourceNotFoundError,
     AlreadyJoinedThisEvent,
-    AlreadyConfirmedEventParticipation
+    AlreadyConfirmedEventParticipation,
+    IsNotParticipatingInThisEvent,
 } = require("../models/errors");
 
-const Event = mongoose.model('Event');
-const EventParticipation = mongoose.model('EventParticipation');
-
+const Event = mongoose.model("Event");
+const EventParticipation = mongoose.model("EventParticipation");
 
 const getAllEvents = async () => {
     //TODO: filters and sorting
     const events = await Event.find({});
 
-    return Promise.all(events.map(async event => await mapEventWithParticipants(event)));
-}
+    return Promise.all(
+        events.map(async (event) => await mapEventWithParticipants(event))
+    );
+};
 
 const getEventById = async (eventId) => {
-    const event = await Event.findOne({id: eventId});
+    const event = await Event.findOne({ id: eventId });
     if (!event) {
-        throw new ResourceNotFoundError('Event with this id not found')
+        throw new ResourceNotFoundError("Event with this id not found");
     }
 
     return mapEventWithParticipants(event);
-}
+};
 
 const findParticipantsForEvents = async (eventId) => {
-    const eventParticipants = await EventParticipation.find({eventId});
+    const eventParticipants = await EventParticipation.find({ eventId });
 
-    return eventParticipants.map(({participantUsername, participationConfirmed}) => ({
-        participantUsername,
-        participationConfirmed
-    }))
-}
+    return eventParticipants.map(
+        ({ participantUsername, participationConfirmed }) => ({
+            participantUsername,
+            participationConfirmed,
+        })
+    );
+};
 
 const mapEventWithParticipants = async (event) => ({
     ...event.toJSON(),
-    participants: await findParticipantsForEvents(event.id)
-})
-
+    participants: await findParticipantsForEvents(event.id),
+});
 
 const createEvent = async (event) => {
-
     // TODO: validate or convert fields?
     const eventToSave = {
         ...event,
-        verified: false
+        verified: false,
     };
 
     const savedEvent = await Event.create(eventToSave);
 
     return await mapEventWithParticipants(savedEvent);
-}
+};
 
 const updateEvent = async (eventId, eventPatches) => {
-
     let existingEvent = getEventById(eventId);
 
     const updatedEvent = {
         ...existingEvent,
-        ...eventPatches
-    }
+        ...eventPatches,
+    };
 
-    const savedEvent = await Event.findOneAndUpdate({id: eventId}, updatedEvent, {new: true});
+    const savedEvent = await Event.findOneAndUpdate(
+        { id: eventId },
+        updatedEvent,
+        { new: true }
+    );
 
     return mapEventWithParticipants(savedEvent);
-}
+};
 
 const joinEvent = async (eventId, participantUsername) => {
     // Check if event exists
     let existingEvent = getEventById(eventId);
 
-    const existingEventParticipation = await EventParticipation.findOne({eventId, participantUsername});
+    const existingEventParticipation = await EventParticipation.findOne({
+        eventId,
+        participantUsername,
+    });
     if (existingEventParticipation) {
-        throw new AlreadyJoinedThisEvent(`Participant ${participantUsername} has already joined event with id ${eventId}`);
+        throw new AlreadyJoinedThisEvent(
+            `Participant ${participantUsername} has already joined event with id ${eventId}`
+        );
     }
 
     const eventParticipation = await EventParticipation.create({
         eventId,
         participantUsername,
-        participationConfirmed: false
-    })
+        participationConfirmed: false,
+    });
 
     return eventParticipation;
+};
 
-}
+const leaveEvent = async (eventId, participantUsername) => {
+    // Check if event exists
+    let existingEvent = await getEventById(eventId);
+
+    const existingEventParticipation = await EventParticipation.findOne({
+        eventId: eventId,
+        participantUsername: participantUsername,
+    });
+
+    if (!existingEventParticipation) {
+        throw new IsNotParticipatingInThisEvent(
+            `Participant ${participantUsername} is not a part of event with id ${eventId}`
+        );
+    }
+
+    await EventParticipation.deleteOne({ eventId, participantUsername });
+};
 
 const confirmEventParticipation = async (eventId, participantUsername) => {
     // Check if event exists
-    const existingEventParticipation = await EventParticipation.findOne({eventId, participantUsername});
+    const existingEventParticipation = await EventParticipation.findOne({
+        eventId,
+        participantUsername,
+    });
 
     if (!existingEventParticipation) {
-        throw ResourceNotFoundError('User has never participated in this event')
+        throw ResourceNotFoundError(
+            "User has never participated in this event"
+        );
     }
     if (existingEventParticipation.participationConfirmed) {
-        throw AlreadyConfirmedEventParticipation('User has never participated in this event')
+        throw AlreadyConfirmedEventParticipation(
+            "User has never participated in this event"
+        );
     }
 
-    return EventParticipation.findOneAndUpdate({
-        eventId,
-        participantUsername
-    }, {participationConfirmed: true}, {new: true});
-}
+    return EventParticipation.findOneAndUpdate(
+        {
+            eventId,
+            participantUsername,
+        },
+        { participationConfirmed: true },
+        { new: true }
+    );
+};
 
+const getEventParticipants = async (eventId) => {
+    const existingEvent = await EventParticipation.find({ eventId: eventId });
+    console.log("event entries:");
+    // console.log(existingEvent);
+    return existingEvent.map((entry) => entry.participantUsername);
+};
+
+const getUsersEvents = async (participantUsername) => {
+    const usersEvents = await EventParticipation.find({
+        participantUsername: participantUsername,
+        participationConfirmed: true,
+    });
+    console.log(usersEvents);
+};
 
 module.exports = {
     getAllEvents,
@@ -113,7 +165,7 @@ module.exports = {
     getEventById,
     updateEvent,
     joinEvent,
-    confirmEventParticipation
-}
-
-
+    leaveEvent,
+    confirmEventParticipation,
+    getEventParticipants,
+};
