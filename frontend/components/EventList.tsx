06fb5cc6db2b4243cc84d47f438a9d756c6a2bf7
pzareba22 <pzareba22@gmail.com>
2022-05-24 14:05:37 +0200

    formatting

--- frontend/components/EventList.tsx
+++ frontend/components/EventList.tsx
@@ -2,37 +2,42 @@ import { StatusBar } from "expo-status-bar";
 import { Alert, ScrollView, StyleSheet, View } from "react-native";
 import EventCard from "./EventCard";
 import { useNavigation } from "@react-navigation/native";
-import {IEvent, IUser} from "../constants";
+import { IEvent, IUser } from "../constants";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import axios from '../axiosconfig';
-import {connect} from "react-redux";
-import {setEventListAction} from "../actions";
-import {useEffect} from "react";
+import axios from "../axiosconfig";
+import { connect } from "react-redux";
+import { setEventListAction } from "../actions";
+import { useEffect } from "react";
 
 interface IProps {
-    events: IEvent[];
-    user: IUser;
-    setEventList: (eventList: IEvent[]) => void;
+  events: IEvent[];
+  user: IUser;
+  setEventList: (eventList: IEvent[]) => void;
 }
-const EventList: React.FC<IProps> = ({events = [], setEventList, user}) => {
+const EventList: React.FC<IProps> = ({ events = [], setEventList, user }) => {
   const navigation =
-    useNavigation<NativeStackNavigationProp<{ Details: { eventId: string } }>>();
+    useNavigation<
+      NativeStackNavigationProp<{ Details: { eventId: string } }>
+    >();
 
-    useEffect(() => {
-        fetchEventList(setEventList);
-    }, [])
+  useEffect(() => {
+    fetchEventList(setEventList);
+  }, []);
 
-    const fetchEventList = (dispatch: (eventList: IEvent[]) => void) => {
-        axios.get(`/events`)
-            .then(({data}) => {
-                dispatch(data);
-            })
-            .catch(err => {
-                console.log(err);
-                Alert.alert("Błąd", "Nie udało się pobrać listy wydarzeń. ", [{text: "OK"}]);
-                return;
-            })
-    }
+  const fetchEventList = (dispatch: (eventList: IEvent[]) => void) => {
+    axios
+      .get(`/events`)
+      .then(({ data }) => {
+        dispatch(data);
+      })
+      .catch((err) => {
+        console.log(err);
+        Alert.alert("Błąd", "Nie udało się pobrać listy wydarzeń. ", [
+          { text: "OK" },
+        ]);
+        return;
+      });
+  };
 
   return (
     <View style={styles.container}>
@@ -42,7 +47,9 @@ const EventList: React.FC<IProps> = ({events = [], setEventList, user}) => {
             <EventCard
               event={entry}
               key={i}
-              click={() => navigation.navigate("Details", { eventId: entry.id })}
+              click={() =>
+                navigation.navigate("Details", { eventId: entry.id })
+              }
             />
           );
           // return <EventCard event={entry} key={i} />;
@@ -59,14 +66,15 @@ const styles = StyleSheet.create({
     backgroundColor: "#f2f2f2",
     alignItems: "center",
     justifyContent: "center",
-    paddingTop: 20,
+    paddingTop: 35,
   },
 });
 const mapStateAsProps = (state: any) => ({
-    events: state.events,
+  events: state.events,
 });
 const mapDispatchToProps = (dispatch: any) => ({
-    setEventList: (eventList: IEvent[]) => dispatch(setEventListAction(eventList)),
+  setEventList: (eventList: IEvent[]) =>
+    dispatch(setEventListAction(eventList)),
 });
 
 export default connect(mapStateAsProps, mapDispatchToProps)(EventList);
